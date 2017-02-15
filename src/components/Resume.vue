b6e5625e47bf2dc2b33ae86cb4900d61cff211a2
Hao Xiangpeng <3150102255@zju.edu.cn>
2017-02-15 21:54:34 +0800

    finished refactor

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -12,28 +12,21 @@
     <div class="other-info">
       <i class="fa fa-github" aria-hidden="true"><a href="https://github.com/HaoPatrick"> Hao Patrick</a></i>|
       <i class="fa fa-envelope" aria-hidden="true"><a href="mailto:haoxiangpeng@hotmail.com"> haoxiangpeng@hotmail.com</a></i>|
-      <i class="fa fa-home" aria-hidden="true"><a href="https://haoxp.xyz"> haoxp.xyz</a></i>|   
-      <i class="fa fa-telegram" aria-hidden="true"><a href="https://telegram.org/"> Haoooxiangpeng</a></i>|         
+      <i class="fa fa-home" aria-hidden="true"><a href="https://haoxp.xyz"> haoxp.xyz</a></i>|
+      <i class="fa fa-telegram" aria-hidden="true"><a href="https://telegram.org/"> Haoooxiangpeng</a></i>|
       <i class="fa fa-linkedin" aria-hidden="true"> <a href="https://www.linkedin.com/in/hao-xiangpeng-15ab33103"> Hao xiangpeng</a></i>
     </div>
     <div class="moto">
       {{mainData.moto}}
     </div>
 
-    <div class="main-title">
-      {{mainData.skillsAbility|toUpper}} 
-      <span class="line">
-      </span>
-    </div>
+    <subtitle :title="mainData.skillsAbility"></subtitle>
     <div class="skill-content">
-        <div class="skill-item" v-for="skill in mainData.skills">{{skill}}</div>
+      <div class="skill-item" v-for="skill in mainData.skills">{{skill}}</div>
     </div>
 
-    <div class="main-title">
-      {{mainData.education|toUpper}} 
-      <span class="line">
-      </span>
-    </div>
+    <subtitle :title="mainData.education"></subtitle>
+
     <div class="sub-main-title">
       {{mainData.school}} ({{mainData.major}})
       <span class="location">{{mainData.location1}}</span>
@@ -45,15 +38,11 @@
     <div>
       <ul class="list-content">
         <li>{{mainData.eduOne}}</li>
-        <li>{{mainData.eduTwo}}</li>        
+        <li>{{mainData.eduTwo}}</li>
       </ul>
     </div>
 
-    <div class="main-title">
-      {{mainData.experience|toUpper}} 
-      <span class="line">
-      </span>
-    </div>
+    <subtitle :title="mainData.experience"></subtitle>
     <div class="sub-main-title">
       {{mainData.expOneTitle}}
       <span class="location">{{mainData.location1}}</span>
@@ -73,11 +62,8 @@
       </ul>
     </div>
 
-    <div class="main-title">
-      {{mainData.project|toUpper}}
-      <span class="line">
-      </span>
-    </div>
+
+    <subtitle :title="mainData.project"></subtitle>
     <div class="sub-main-title">
       {{mainData.projectTmpylee}}
       <span class="location">{{mainData.location1}}</span>
@@ -113,166 +99,189 @@
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  computed: {
-    mainData: function () {
-      if (this.language === 'En') {
-        return {
-          name: 'Hao Xiangpeng',
-          major: 'Computer Science',
-          area: 'Web Enginnering',
-          address: 'Zijingang Campus, Zhejiang University, Hangzhou, Zhejiang Province, China',
-          moto: '"Who end the acoustic shock the world, will be a long time deep self silence"',
-          education: 'Education',
-          school: 'Zhejiang University and Simon Fraser University',
-          location1: 'Hangzhou, China',
-          degree: "Bachelor's degree in computer science",
-          duration1: 'Sept. 2015 - PRESENT',
-          duration2: 'Sept. 2016 - PRESENT',
-          eduOne: 'First two years at Zhejiang University in China majoring Computer Science.',
-          eduTwo: 'Simon Fraser University majoring Computer Science',
-          experience: 'experience',
-          skillsAbility: 'skills/abilities',
-          skills: ['Linux Server', 'Python', 'C++', 'JavaScript', 'Git'],
-          expOneTitle: 'QSC Zhejiang University',
-          expOnePos: 'Chief Technology Officer',
-          qscwork: 'Web Frontend/Backend',
-          qscwork1: 'Freshman Mannual for Zhejiang University. Use pure css and js without any framwork, integrated various kinds of service to provide excellent user experience',
-          qscwork2: 'Welcome Page for Recruitment in 2016. Use Vue.js and Bootstrap to boost developing efficiency',
-          qscwork3: 'Recruiting Manage System. Python (Django) as the backend server, Bootstrap and Webpack to imporve the performance of such a large project',
-          qscwork4: 'Linux Server management. Docker, Shell, Git, Continuous Integration and Test Driven Develop',
-          qscwork5: 'Telegram Bot. A bot to record all the message in the group and get statastic message everyday.',
-          project: 'project',
-          projectTmpylee: 'Tmplaye',
-          tmplayedesc: 'simple yet powerful',
-          duration3: 'Jan. 2017',
-          tmplaye1: 'A Python3.6 project',
-          tmplaye2: "A template engine similar to Django's",
-          tmplaye3: 'Support various inline expression and complex condition control',
-          projectHouaa: 'Houaa',
-          duration4: 'Feb. 2017 - Present',
-          houaa2: 'Vue.js + Vue-Router + Vuex + Webpack',
-          houaa3: 'Server Architect',
-          houaa1: 'A Education Sharing Platform'
+  import subtitle from './ResumeTitle'
+  export default {
+    name: 'hello',
+    components: {
+      subtitle
+    },
+    computed: {
+      mainData: function () {
+        if (this.language === 'En') {
+          return {
+            name: 'Hao Xiangpeng',
+            major: 'Computer Science',
+            area: 'Web Enginnering',
+            address: 'Zijingang Campus, Zhejiang University, Hangzhou, Zhejiang Province, China',
+            moto: '"Who end the acoustic shock the world, will be a long time deep self silence"',
+            education: 'Education',
+            school: 'Zhejiang University and Simon Fraser University',
+            location1: 'Hangzhou, China',
+            degree: "Bachelor's degree in computer science",
+            duration1: 'Sept. 2015 - PRESENT',
+            duration2: 'Sept. 2016 - PRESENT',
+            eduOne: 'First two years at Zhejiang University in China majoring Computer Science.',
+            eduTwo: 'Simon Fraser University majoring Computer Science',
+            experience: 'experience',
+            skillsAbility: 'skills/abilities',
+            skills: ['Linux Server', 'Python', 'C++', 'JavaScript', 'Git'],
+            expOneTitle: 'QSC Zhejiang University',
+            expOnePos: 'Chief Technology Officer',
+            qscwork: 'Web Frontend/Backend',
+            qscwork1: 'Freshman Mannual for Zhejiang University. Use pure css and js without any framwork, integrated various kinds of service to provide excellent user experience',
+            qscwork2: 'Welcome Page for Recruitment in 2016. Use Vue.js and Bootstrap to boost developing efficiency',
+            qscwork3: 'Recruiting Manage System. Python (Django) as the backend server, Bootstrap and Webpack to imporve the performance of such a large project',
+            qscwork4: 'Linux Server management. Docker, Shell, Git, Continuous Integration and Test Driven Develop',
+            qscwork5: 'Telegram Bot. A bot to record all the message in the group and get statastic message everyday.',
+            project: 'project',
+            projectTmpylee: 'Tmplaye',
+            tmplayedesc: 'simple yet powerful',
+            duration3: 'Jan. 2017',
+            tmplaye1: 'A Python3.6 project',
+            tmplaye2: "A template engine similar to Django's",
+            tmplaye3: 'Support various inline expression and complex condition control',
+            projectHouaa: 'Houaa',
+            duration4: 'Feb. 2017 - Present',
+            houaa2: 'Vue.js + Vue-Router + Vuex + Webpack',
+            houaa3: 'Server Architect',
+            houaa1: 'A Education Sharing Platform'
+          }
         }
       }
-    }
-  },
-  filters: {
-    toUpper: function (value) {
-      if (!value) return ''
-      value = value.toString()
-      return value.toUpperCase()
-    }
-  },
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      language: 'En'
+    },
+    filters: {
+      toUpper: function (value) {
+        if (!value) return ''
+        value = value.toString()
+        return value.toUpperCase()
+      }
+    },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        language: 'En'
+      }
     }
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.skill-content ul {
-  list-style-type: none;
-}
-.skill-content{
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-around;
-  margin-top: .7rem;
-}
-.skill-item{
-  margin-right: 2rem;
-  font-size: 1.2rem;
-  /*flex-grow: 1;*/
-}
-.list-content{
-  padding-left: 1.1rem;
-  margin-top: 0.4rem;
-  font-weight: lighter;
-  font-size: 0.9rem;
-}
-.sub-content{
-  font-size: 0.8rem;
-}
-.duration{
-  float: right;
-  font-weight: lighter;
-  font-style: italic;
-}
-.sub-main-title{
-  font-weight: 600;
-  font-size: 1rem;
-  margin-bottom: 0.5rem;
-}
-.location{
-  font-weight: normal;
-  font-size: 0.9rem;
-  font-style: italic;
-  float: right;
-  color: #e74c3c;
-}
-.line{
-  border-bottom: 0.1rem solid #2c3e50;
-  width: 100%;
-  margin: auto auto 0.4rem 0.2rem;
-}
-.main-title{
-  font-size: 1.4rem;
-  font-weight: bold;
-  color: #e74c3c;
-  display: flex;
-  margin-bottom: 0.2rem;
-  margin-top: 2rem;
-}
-.moto{
-  color: #2c3e50;
-  text-align: center;
-  margin-top: 1rem;
-  margin-bottom: 1rem;
-  font-style: italic;
-}
- a{
-  text-decoration: none;
-  color: #2c3e50;
-}
- a:visited{
-  text-decoration: none;
-  color: #2c3e50;
-}
-.fa{
-  margin: 0 0.5rem 0 0.5rem;
-}
-.other-info{
-  font-size: 0.7rem;
-  text-align: center;
-}
-.address{
-  text-align: center;
-  font-size: 0.7rem;
-  font-style: italic;
-  color: #7f8c8d;
-  margin-bottom: 0.3rem;
-  margin-top: 0.3rem;
-}
-.sub-title{
-  color: #e74c3c;
-  font-size: 0.7rem;
-  text-align: center;
-}
-.main-container{
-  margin: 60px 10% auto 10%;
-}
-.re-name{
-  font-size: 2.6rem;
-  font-weight: bold;
-  margin: 0 auto 0.5rem auto;
-  width:100%;
-  text-align: center;
-}
+  .skill-content ul {
+    list-style-type: none;
+  }
+  
+  .skill-content {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+    margin-top: .7rem;
+  }
+  
+  .skill-item {
+    margin-right: 2rem;
+    font-size: 1.2rem;
+    /*flex-grow: 1;*/
+  }
+  
+  .list-content {
+    padding-left: 1.1rem;
+    margin-top: 0.4rem;
+    font-weight: lighter;
+    font-size: 0.9rem;
+  }
+  
+  .sub-content {
+    font-size: 0.8rem;
+  }
+  
+  .duration {
+    float: right;
+    font-weight: lighter;
+    font-style: italic;
+  }
+  
+  .sub-main-title {
+    font-weight: 600;
+    font-size: 1rem;
+    margin-bottom: 0.5rem;
+  }
+  
+  .location {
+    font-weight: normal;
+    font-size: 0.9rem;
+    font-style: italic;
+    float: right;
+    color: #e74c3c;
+  }
+  
+  .line {
+    border-bottom: 0.1rem solid #2c3e50;
+    width: 100%;
+    margin: auto auto 0.4rem 0.2rem;
+  }
+  
+  .main-title {
+    font-size: 1.4rem;
+    font-weight: bold;
+    color: #e74c3c;
+    display: flex;
+    margin-bottom: 0.2rem;
+    margin-top: 2rem;
+  }
+  
+  .moto {
+    color: #2c3e50;
+    text-align: center;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    font-style: italic;
+  }
+  
+  a {
+    text-decoration: none;
+    color: #2c3e50;
+  }
+  
+  a:visited {
+    text-decoration: none;
+    color: #2c3e50;
+  }
+  
+  .fa {
+    margin: 0 0.5rem 0 0.5rem;
+  }
+  
+  .other-info {
+    font-size: 0.7rem;
+    text-align: center;
+  }
+  
+  .address {
+    text-align: center;
+    font-size: 0.7rem;
+    font-style: italic;
+    color: #7f8c8d;
+    margin-bottom: 0.3rem;
+    margin-top: 0.3rem;
+  }
+  
+  .sub-title {
+    color: #e74c3c;
+    font-size: 0.7rem;
+    text-align: center;
+  }
+  
+  .main-container {
+    margin: 60px 10% auto 10%;
+  }
+  
+  .re-name {
+    font-size: 2.6rem;
+    font-weight: bold;
+    margin: 0 auto 0.5rem auto;
+    width: 100%;
+    text-align: center;
+  }
 </style>
---
+++ src/components/ResumeTitle.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="main-title">
+    <span>{{title|toUpper|getFirst}}</span><span style="color: #2c3e50">{{title|toUpper|getRemain}}</span>
+    <span class="line">
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    props: [
+      'title'
+    ],
+    filters: {
+      toUpper: function (value) {
+        if (!value) return ''
+        value = value.toString()
+        return value.toUpperCase()
+      },
+      getFirst: function (value) {
+        return value.slice(0, 1)
+      },
+      getRemain: function (value) {
+        return value.slice(1)
+      }
+    },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .line {
+    border-bottom: 0.1rem solid #2c3e50;
+    width: 100%;
+    margin: auto auto 0.4rem 0.2rem;
+  }
+  
+  .main-title {
+    font-size: 1.4rem;
+    font-weight: bold;
+    color: #e74c3c;
+    display: flex;
+    margin-bottom: 0.2rem;
+    margin-top: 2rem;
+  }
+</style>
