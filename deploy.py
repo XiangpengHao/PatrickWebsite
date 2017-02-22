a8080a38592fcb392667f69330e85c95a364bf6f
Hao Xiangpeng <3150102255@zju.edu.cn>
2017-02-22 16:15:39 +0800

    can deploy to firebase

--- deploy.py
+++ deploy.py
@@ -3,3 +3,4 @@ from subprocess import call
 call(['npm','run','build'])
 call(['rsync','-avzhe','ssh','dist/index.html','root@haoxp.xyz:/usr/share/nginx/resume'])
 call(['rsync','-avzhe','ssh','dist/static','root@haoxp.xyz:/usr/share/nginx/resume'])
+call(['firebase', 'deploy'])
