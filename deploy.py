6a7ca92fff5ccd961eb31c27e5103e52f87b613a
haoxiangpeng <3150102255@zju.edu.cn>
2017-08-06 05:51:01 +0000

    service worker

--- deploy.py
+++ deploy.py
@@ -1,6 +1,6 @@
 from subprocess import call
 
 call(['npm','run','build'])
-call(['rsync','-avzhe','ssh','dist/index.html','root@haoxp.xyz:/usr/share/nginx/resume'])
-call(['rsync','-avzhe','ssh','dist/static','root@haoxp.xyz:/usr/share/nginx/resume'])
-call(['firebase', 'deploy'])
+call(['rsync','-avzhe','ssh','dist/','root@haoxp.xyz:/usr/share/nginx/resume'])
+#call(['rsync','-avzhe','ssh','dist/static','root@haoxp.xyz:/usr/share/nginx/resume'])
+#call(['firebase', 'deploy'])
