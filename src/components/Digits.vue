30008852be5174725acbe8343d595ce27f2c2b03
haoxiangpeng <3150102255@zju.edu.cn>
2017-08-10 12:28:18 +0000

    add neuron net work

--- src/components/Digits.vue
+++ src/components/Digits.vue
@@ -2,7 +2,12 @@
   <div class="hello">
     <div style="display:flex;">
       <canvas v-on:mousemove="mouseMove" v-on:click="paint" id="canvas" width="257" height="257"></canvas>
-      <div style="margin-left:1em;">Result is: {{result}}</div>
+      <transition name="el-zoom-in-center">
+        <div v-if="result" style="flex-direction:column;">
+          <div style="margin-left:1em;">KNN gets a {{result.result32}}</div>
+          <div style="margin-left:1em;">While neuron network gets a {{result.result28}}</div>
+        </div>
+      </transition>
     </div>
     <div style="margin-top:1em;">
       <el-button size="small" @click="reset">Reset</el-button>
@@ -20,6 +25,7 @@ export default {
       canvas: '',
       ctx: '',
       drawData: [],
+      data28: [],
       result: ''
     }
   },
@@ -48,15 +54,29 @@ export default {
         }
       }
     },
-    setData: function (n) {
-      this.$set(this.drawData, n, 1)
+    setData: function (x, y) {
+      let x32 = Math.floor(x / 8)
+      let x28 = Math.floor(x / 9.14)
+      let y32 = Math.floor(y / 8)
+      let y28 = Math.floor(y / 9.14)
+
+      this.$set(this.drawData, x32 + 32 * y32, 1)
+      this.$set(this.drawData, x32 + 32 * y32 - 1, 1)
+      this.$set(this.drawData, x32 + 32 * y32 + 1, 1)
+      this.$set(this.drawData, x32 + 31 * y32, 1)
+      this.$set(this.drawData, x32 + 33 * y32, 1)
+
+      this.$set(this.data28, x28 + 28 * y28, 1)
+      this.$set(this.data28, x28 + 28 * y28 - 1, 1)
+      this.$set(this.data28, x28 + 28 * y28 + 1, 1)
+      this.$set(this.data28, x28 + 27 * y28, 1)
+      this.$set(this.data28, x28 + 29 * y28, 1)
     },
     drawPixel: function (x, y) {
       let ctx = this.ctx
       // ctx.beginPath(x * 8 + y * 8)
       ctx.fillStyle = '#e74c3c'
       ctx.fillRect(x * 8 + 1, y * 8 + 1, 7, 7)
-      this.setData(x + y * 32)
     },
     drawTrace: function (x, y) {
       this.drawPixel(x, y)
@@ -70,12 +90,11 @@ export default {
       let offsetX = e.offsetX
       let offsetY = e.offsetY
       this.drawTrace(Math.floor(offsetX / 8), Math.floor(offsetY / 8))
+      this.setData(offsetX, offsetY)
     },
     mouseMove: function (e) {
       if (e.which === 1) {
-        let offsetX = e.offsetX
-        let offsetY = e.offsetY
-        this.drawTrace(Math.floor(offsetX / 8), Math.floor(offsetY / 8))
+        this.paint(e)
       }
     },
     reset: function () {
@@ -83,19 +102,29 @@ export default {
       this.initCanvas()
       this.drawData = []
       this.result = ''
+      this.data28 = []
     },
     submit: function () {
       let self = this
-      let data = this.drawData
+      let data32 = this.drawData
       for (let i = 0; i < 1024; i++) {
-        data[i] = data[i] ? 1 : 0
+        data32[i] = data32[i] ? 1 : 0
       }
+      data32 = data32.slice(0, 1024)
+
+      let data28 = this.data28
+      for (let i = 0; i < 784; i++) {
+        data28[i] = data28[i] ? 1 : 0
+      }
+      data28 = data28.slice(0, 784)
+
       // console.log(data)
       axios({
         method: 'post',
         url: 'https://digits.haoxp.xyz/digit',
         data: {
-          'drawData': data
+          'data32': data32,
+          'data28': data28
         }
       }).then(
         response => {
