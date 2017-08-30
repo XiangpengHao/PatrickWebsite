cc52c4ed32e943368eca8cd3446fa9721b9f1249
haoxiangpeng <3150102255@zju.edu.cn>
2017-08-30 18:55:40 +0000

    change homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <div style="width:100%;height:100%;position:absolute;">
-    <div style="z-index:-1;background-color:#c0392b;position:absolute;width:100%;height:100%;">
-      <vue-particles color="#dedede" :particleOpacity="0.7" :particlesNumber="80" shapeType="circle" :particleSize="4" linesColor="#dedede" :linesWidth="1" :lineLinked="true" :lineOpacity="0.4" :linesDistance="150" :moveSpeed="3" :hoverEffect="false" hoverMode="grab" :clickEffect="false" clickMode="push">
+  <div style="width:100%;height:100%;position:absolute;overflow:hidden;">
+    <div style="background-color:#AD002B;position:relative;width:100%;height:100%;">
+      <vue-particles color="#dedede" :particleOpacity="0.7" :particlesNumber="80" shapeType="circle" :particleSize="4" linesColor="#dedede" :linesWidth="1" :lineLinked="true" :lineOpacity="0.4" :linesDistance="150" :moveSpeed="3" :hoverEffect="true" hoverMode="grab" :clickEffect="true" clickMode="push">
       </vue-particles>
     </div>
-    <div style="width:100%;height:100%;display:flex;flex-direction:column;  justify-content: center;">
-      <h1 style="color:#eee;text-align:center;">
+    <div style="font-family:'Open Sans', sans-serif;;position:absolute;top:40%;left:50%;transform: translate(-50%, -50%);color:#eee;text-align:center;">
+      <h1 style="font-size:2.5em;">
         Hao Xiangpeng
       </h1>
-      <div style="height:22%;"></div>
+      <div style="font-family:'Slabo 27px', serif;font-size:1.2em;">
+        <ul style="display:inline;list-style:none;margin:0;">
+          <li class="inline-li">Who end the acoustic shock the world,</li>
+          <li class="inline-li">will be a long time from deep silence</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -33,5 +38,22 @@ body {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#particles-js {
+  width: 100%;
+  height: 100%;
+}
 
+ul {
+  margin: 0;
+  -webkit-margin-before: 0;
+  -webkit-margin-after: 0;
+  -webkit-margin-start: 0px;
+  -webkit-margin-end: 0px;
+  -webkit-padding-start: 0;
+}
+
+.inline-li {
+  display: inline;
+  white-space: nowrap;
+}
 </style>
