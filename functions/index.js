3960161a5c2aef51637849f7cbf276560b49b93a
haoxiangpeng <3150102255@zju.edu.cn>
2017-11-26 18:24:01 -0800

    :sparkles: add thumbnial function

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,124 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for t`he specific language governing permissions and
+ * limitations under the License.
+ */
+
 const functions = require('firebase-functions')
+const mkdirp = require('mkdirp-promise')
+// Include a Service Account Key to use a Signed URL
+const gcs = require('@google-cloud/storage')({
+  keyFilename: 'service-account-credentials.json'
+})
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-exports.addMessage = functions.https.onRequest((req, res) => {
-  const original = req.query.text
-  admin.database().ref('/messages').push({
-    original: original
-  }).then(snapshot => {
-    res.redirect(300, snapshot.ref)
-  })
+const spawn = require('child-process-promise').spawn
+const path = require('path')
+const os = require('os')
+const fs = require('fs')
+
+// Max height and width of the thumbnail in pixels.
+const THUMB_MAX_HEIGHT = 200
+const THUMB_MAX_WIDTH = 200
+// Thumbnail prefix added to file names.
+const THUMB_PREFIX = 'thumb_'
+
+/**
+ * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
+ * ImageMagick.
+ * After the thumbnail has been generated and uploaded to Cloud Storage,
+ * we write the public URL to the Firebase Realtime Database.
+ */
+exports.generateThumbnail = functions.storage.object().onChange(event => {
+  // File and directory paths.
+  const filePath = event.data.name
+  const contentType = event.data.contentType // This is the image Mimme type
+  const fileDir = path.dirname(filePath)
+  const fileName = path.basename(filePath)
+  const thumbFilePath = path.normalize(path.join(fileDir, `${THUMB_PREFIX}${fileName}`))
+  const tempLocalFile = path.join(os.tmpdir(), filePath)
+  const tempLocalDir = path.dirname(tempLocalFile)
+  const tempLocalThumbFile = path.join(os.tmpdir(), thumbFilePath)
+
+  // Exit if this is triggered on a file that is not an image.
+  if (!contentType.startsWith('image/')) {
+    console.log('This is not an image.')
+    return null
+  }
+
+  // Exit if the image is already a thumbnail.
+  if (fileName.startsWith(THUMB_PREFIX)) {
+    console.log('Already a Thumbnail.')
+    return null
+  }
+
+  // Exit if this is a move or deletion event.
+  if (event.data.resourceState === 'not_exists') {
+    console.log('This is a deletion event.')
+    return null
+  }
+
+  // Cloud Storage files.
+  const bucket = gcs.bucket(event.data.bucket)
+  const file = bucket.file(filePath)
+  const thumbFile = bucket.file(thumbFilePath)
+  const metadata = {
+    contentType: contentType
+  }
+
+  // Create the temp directory where the storage file will be downloaded.
+  return mkdirp(tempLocalDir).then(() => {
+    // Download file from bucket.
+    return file.download({
+      destination: tempLocalFile
+    })
+  }).then(() => {
+    console.log('The file has been downloaded to', tempLocalFile)
+    // Generate a thumbnail using ImageMagick.
+    return spawn('convert', [tempLocalFile, '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempLocalThumbFile], {
+      capture: ['stdout', 'stderr']
+    })
+  }).then(() => {
+    console.log('Thumbnail created at', tempLocalThumbFile)
+    // Uploading the Thumbnail.
+    return bucket.upload(tempLocalThumbFile, {
+      destination: thumbFilePath,
+      metadata: metadata
+    })
+  }).then(() => {
+    console.log('Thumbnail uploaded to Storage at', thumbFilePath)
+    // Once the image has been uploaded delete the local files to free up disk space.
+    fs.unlinkSync(tempLocalFile)
+    fs.unlinkSync(tempLocalThumbFile)
+    // Get the Signed URLs for the thumbnail and original image.
+    const config = {
+      action: 'read',
+      expires: '03-01-2500'
+    }
+    return Promise.all([
+      thumbFile.getSignedUrl(config),
+      file.getSignedUrl(config)
+    ])
+  }).then(results => {
+    console.log('Got Signed URLs.')
+    const thumbResult = results[0]
+    const originalResult = results[1]
+    const thumbFileUrl = thumbResult[0]
+    const fileUrl = originalResult[0]
+    // Add the URLs to the Database
+    return admin.database().ref('images').push({
+      path: fileUrl,
+      thumbnail: thumbFileUrl
+    })
+  }).then(() => console.log('Thumbnail URLs saved to database.'))
 })
