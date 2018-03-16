f673e84ab40d8154d1ac76ef617fce75aa66df19
Xiangpeng Hao <haoxiangpeng123@gmail.com>
2018-03-15 20:57:38 -0700

    :art: work on homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@
       </div>
     </div>
     <div style="position:absolute;top:50%;left:50%;transform: translate(-50%, -30%);color:#eee;text-align:center;">
-      <h1 style="font-size:2.5em;margin-bottom:0.2em;">
+      <h1 style="font-size:4em;margin-bottom:0.2em;">
         Xiangpeng Hao
       </h1>
       <div style="font-family:'Roboto Slab', serif;font-size:1.2em;">
@@ -19,50 +19,39 @@
           <li>
             <a href="https://github.com/HaoPatrick">
               <el-tooltip class="item" effect="dark" content="GitHub" placement="top">
-                <i class="fa fa-github" aria-hidden="true">
-                </i>
+                <img class="icons" src="../assets/icons/github.svg">
               </el-tooltip>
             </a>
           </li>
           <li>
-            <router-link to="photos">
-              <el-tooltip class="item" effect="dark" content="Photography" placement="top">
-                <i class="fa fa-camera fa-4" aria-hidden="true">
-                </i>
-              </el-tooltip>
-            </router-link>
-          </li>
-          <li>
             <router-link to="resume">
               <el-tooltip class="item" effect="dark" content="Resume" placement="top">
-                <i class="fa fa-address-card" aria-hidden="true"></i>
+                <img class="icons" src="../assets/icons/address-card.svg">
               </el-tooltip>
             </router-link>
           </li>
           <li>
             <a href="https://blog.haoxp.xyz">
-              <el-tooltip class="item" effect="dark" content="Blog" placement="top">
-                <i class="fa fa-rss-square" aria-hidden="true"></i>
-              </el-tooltip>
+                <img class="icons" src="../assets/icons/blogger-b.svg">
             </a>
-          </li>
-          <li>
-            <router-link to="iot">
-              <el-tooltip class="item" effect="dark" content="IoT Lab" placement="top">
-                <i class="fa fa-flask" aria-hidden="true"></i>
-              </el-tooltip>
-            </router-link>
           </li>
           <li>
             <router-link to="playground">
               <el-tooltip class="item" effect="dark" content="Playground" placement="top">
-                <i class="fa fa-terminal" aria-hidden="true"> </i>
+                <img class="icons" src="../assets/icons/flask.svg">
+              </el-tooltip>
+            </router-link>
+          </li>
+          <li>
+            <router-link to="photos">
+              <el-tooltip class="item" effect="dark" content="Photography" placement="top">
+                <img class="icons" src="../assets/icons/camera.svg">
               </el-tooltip>
             </router-link>
           </li>
         </ul>
-        <div style="font-size:0.7em;margin-top:0.6em;">
-          <span style="border-top:1px solid #eee;padding:0 1em 0 1em;text-align:center;">© 2012-{{year}} Hao Xiangpeng</span>
+        <div style="font-size:0.7em;">
+          <span style="border-top:1px solid #eee;padding:0 1em 0 1em;text-align:center;">© 2012-{{year}} Xiangpeng Hao</span>
         </div>
       </div>
     </div>
@@ -110,6 +99,19 @@ body {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+:root {
+  --icon-fill-color: #fefefe;
+}
+.icons {
+  min-width: 1em;
+  min-width: 1.2em;
+  transition: all 0.5s;
+}
+
+.icons:hover {
+  transform: translate(0, -0.4em);
+  min-width: 1.5em;
+}
 #particles-js {
   width: 100%;
   height: 100%;
@@ -131,18 +133,23 @@ h1 {
 }
 
 .icon-ul {
-  margin-top: 15em;
-  margin-bottom: 5em;
+  margin-top: 13em;
+  margin-bottom: 9em;
+  font-family: "Noto Sans", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 .icon-ul > ul {
   display: flex;
+  min-height: 4em;
   justify-content: center;
   padding-left: 0px;
 }
 .icon-ul > ul > li {
   display: inline;
-  margin-right: 0.4em;
-  margin-left: 0.4em;
+  margin-right: 0.6em;
+  margin-left: 0.6em;
+  font-size: 2em;
 }
 
 .inline-li {
