075926b3ff9d0a4ea2ec9a38f6b12a8b8119138a
Xiangpeng Hao <xha62@sfu.ca>
2019-07-24 20:09:38 -0700

    default home page to about

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 import Resume from "./views/Resume.vue";
+import About from "./views/About.vue";
 
 Vue.use(Router);
 
@@ -12,7 +12,7 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: About
     },
     {
       path: "/resume",
