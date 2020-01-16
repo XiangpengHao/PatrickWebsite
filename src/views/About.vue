6c3985d384f8a1a07586197dd4a90ed3fcb56568
Xiangpeng Hao <xha62@sfu.ca>
2020-01-15 19:13:13 -0800

    update pub

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,25 +1,18 @@
 <template>
   <div class="about">
     <h1>Xiangpeng Hao</h1>
-    <img
-      style="max-width: 300px"
-      alt="Xiangpeng Hao"
-      src="../assets/photos/xiangpeng_hao.jpg"
-    />
+    <img style="max-width: 300px" alt="Xiangpeng Hao" src="../assets/photos/xiangpeng_hao.jpg" />
     <article>
       <p>
         My research interests lie broadly in software-hardware co-design systems, data-intensive systems, and
         the application of machine learning systems. In particular, I am passionate about projects that have the
         potential to solve real-world problems and aim to impact both the academia and the industry.
       </p>
-      <p style="font-weight: 700;">
-        I'm looking for a Ph.D. position in active system groups with experienced mentors and motivated colleagues :)
-      </p>
+      <p
+        style="font-weight: 700;"
+      >I'm looking for a Ph.D. position in active system groups with experienced mentors and motivated colleagues :)</p>
       <div>
-        <div
-          style="cursor:pointer;"
-          @click="toggleShow"
-        >more...</div>
+        <div style="cursor:pointer;" @click="toggleShow">more...</div>
         <div v-show="showMore">
           My previous research interests was Computer Vision,
           where I researched on colour constancy.
@@ -28,30 +21,30 @@
         </div>
       </div>
       <div class="links">
-        <a
-          :href="`${publicPath}CV_Xiangpeng_Hao.pdf`"
-          target="_blank"
-        >CV</a>
-        <a
-          href="https://blog.haoxp.xyz"
-          target="_blank"
-        >Blog</a>
-        <a
-          href="https://github.com/XiangpengHao"
-          target="_blank"
-        >GitHub</a>
+        <a :href="`${publicPath}CV_Xiangpeng_Hao.pdf`" target="_blank">CV</a>
+        <a href="https://blog.haoxp.xyz" target="_blank">Blog</a>
+        <a href="https://github.com/XiangpengHao" target="_blank">GitHub</a>
       </div>
     </article>
     <article style="margin: 2em 0 2em 0;">
       <div style="font-weight:600;">Publications:</div>
+
+      <div class="pub">
+        <div>DASH: Dynamic and Scalable Hashing on Persistent Memory .</div>
+        <div>
+          Baotong LU,
+          <span style="font-weight: 600">Xiangpeng Hao</span>, Tianzheng Wang, Eric Lo.
+        </div>
+        <div>45th International Conference on Very Large Data Bases (VLDB 2020)</div>
+        <span style="font-style:italic">shepherding</span>
+      </div>
       <div class="pub">
         <div>Evaluating Persistent Memory based Range Indexes</div>
         <div>
-          Lucas Lersch, <span style="font-weight: 600">Xiangpeng Hao</span>, Ismail Oukid, Tianzheng Wang, Thomas Willhalm.
+          Lucas Lersch,
+          <span style="font-weight: 600">Xiangpeng Hao</span>, Ismail Oukid, Tianzheng Wang, Thomas Willhalm.
         </div>
-        <div>
-          45th International Conference on Very Large Data Bases (VLDB 2020)
-        </div>
+        <div>45th International Conference on Very Large Data Bases (VLDB 2020)</div>
       </div>
       <div class="pub">
         <div>A Multi-illuminant Synthetic Image Test Set</div>
@@ -72,14 +65,14 @@
       </div>
     </article>
     <article style="margin: 2em 0 2em 0;">
-      <div style="font-weight:600;">On-going Researchs(Publications):</div>
-      <div class="project">
-        <div>1. Dynamic Hash Table on Persistent Memory</div>
-        <div>Proposed a new approach to build dynamic and scalable hash table on real PM hardware. It achieves scalability by avoiding unnecessary PM access, and can achieve up to 3x better performance than the state-of-the-art.</div>
-      </div>
-      <div class="project">
-        <div>3. Open-source BzTree Implementation</div>
-        <div>Implemented a fully-functional BzTree in C++ and benchmarked on both main memory and persistent memory. Extended PMwCAS to allow safe allocation, and also extended its API so that PMwCAS can support more real world use cases.</div>
+      <div style="font-weight:600;">On-going Researchs (Publications):</div>
+      <div class="pub">
+        <div>PiBench Online: Interactive Benchmarking of Persistent Memory Indexes</div>
+        <div>
+          <span style="font-weight: 600">Xiangpeng Hao</span>
+          , Lucas Lersch, Tianzheng Wang, Ismail Oukid.
+        </div>
+        <div>ACM SIGMOD/PODS International Conference on Management of Data (SIGMOD ’20)</div>
       </div>
     </article>
     <article style="margin: 2em 0 2em 0;">
