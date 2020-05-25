32231ef6319a55c45ada12218f85453f8e17d0b2
Xiangpeng Hao <xha62@sfu.ca>
2020-05-25 14:10:35 -0700

    remove unnecessary things

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Resume from "./views/Resume.vue";
 import About from "./views/About.vue";
 
 Vue.use(Router);
@@ -13,25 +12,6 @@ export default new Router({
       path: "/",
       name: "home",
       component: About
-    },
-    {
-      path: "/resume",
-      name: "resume",
-      component: Resume
-    },
-    {
-      path: "/cv",
-      name: "cv",
-      component: Resume
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
 });
