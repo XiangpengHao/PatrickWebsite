319973f7fbd9a107c20e47596b0971a6ade5202b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-01 03:39:52 +0000

    Deploy XiangpengHao/PatrickWebsite to github.com/XiangpengHao/PatrickWebsite.git:gh-pages

---
+++ precache-manifest.e1db24fd104a21183e4a5098ef222b0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6a5cf2a6ee0041721a8",
+    "url": "/css/app.ffb24286.css"
+  },
+  {
+    "revision": "f6a5cf2a6ee0041721a8",
+    "url": "/js/app.692be3bb.js"
+  },
+  {
+    "revision": "c0d897a18ec8feb7adb2",
+    "url": "/js/chunk-vendors.71d0cd9b.js"
+  },
+  {
+    "revision": "9a0b4747c09961e4934ee2a1cb901102",
+    "url": "/img/xiangpeng_hao.9a0b4747.jpg"
+  },
+  {
+    "revision": "582333cdfbdf64a8f0b63a6c678a3e99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d01c86801756ba88d184de178c350a",
+    "url": "/CV_Xiangpeng_Hao.pdf"
+  },
+  {
+    "revision": "8c7e6b0d9ab260587e8ba3a9a949a345",
+    "url": "/favicon.png"
+  }
+];
